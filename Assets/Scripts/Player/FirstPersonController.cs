9ef072fea7235272cafa6e0e3f997f767259ada8
sabatiny <sabatinystopa@gmail.com>
2023-12-09 22:42:38 -0300

    Adding package

---
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+
+namespace EmployerOfTheMonth.Player
+{
+    public class FirstPersonController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private float mouseSensitive = 100f;
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private Camera cam;
+        private float xRotation = 0f;
+
+        private void Update()
+        {
+            MovementHandler();
+            MouseLook();
+        }
+
+        private void MouseLook()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensitive * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensitive * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Math.Clamp(xRotation, -90f, 90f);
+
+            cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
+        }
+
+        private void MovementHandler()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            var moveDirection = transform.TransformDirection(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+
+            characterController.Move(moveDirection);
+        }
+    }
+}
