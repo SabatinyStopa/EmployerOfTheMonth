5f8527692d8b28f483fe3a915f137947774384ea
sabatiny <sabatinystopa@gmail.com>
2023-12-28 11:43:31 -0300

    Tweak the interactions for a better handling

--- Assets/Scripts/Common/Interactions.cs
+++ Assets/Scripts/Common/Interactions.cs
@@ -14,41 +14,63 @@ namespace EmployerOfTheMonth.Player
 
         private void Update()
         {
+            GrabItemsHandler();
+
+            pickupPoint.transform.Rotate(pickupPoint.transform.up * Input.mouseScrollDelta.y * 50, Space.Self);
+        }
+
+        private void GrabItemsHandler()
+        {
             var ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
             var hitted = Physics.Raycast(ray, out RaycastHit hit, 2f, layerMask);
 
+            SetOutlineToTheCurrentLooking(hitted, hit);
+
+            interactionText.enabled = hitted && currentItemBody == null;
+
+            if (hitted && Input.GetMouseButtonDown(0) && currentItemBody == null)
+                OnClickToGrabItem(hit);
+            else if (Input.GetMouseButtonDown(0) && currentItemBody != null)
+                OnClickToReleaseItem();
+        }
+
+        private void OnClickToReleaseItem()
+        {
+            currentItemBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            currentItemBody.constraints = RigidbodyConstraints.None;
+            currentItemBody.transform.SetParent(null);
+            currentItemBody.useGravity = true;
+            currentItemBody = null;
+        }
+
+        private void OnClickToGrabItem(RaycastHit hit)
+        {
+            currentItemBody = hit.transform.GetComponent<Rigidbody>();
+
+            pickupPoint.rotation = Quaternion.identity;
+
+            currentItemBody.transform.SetParent(pickupPoint);
+            currentItemBody.velocity = Vector3.zero;
+            currentItemBody.useGravity = false;
+            currentItemBody.constraints = RigidbodyConstraints.FreezePosition;
+            currentItemBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            currentItemBody.transform.localPosition = Vector3.zero;
+
+            currentItemBody.GetComponent<Renderer>().materials[1].SetFloat("_OutlineThick", 0f);
+        }
+
+        private void SetOutlineToTheCurrentLooking(bool hitted, RaycastHit hit)
+        {
             if (lookingTo != null)
             {
                 lookingTo.GetComponent<Renderer>().materials[1].SetFloat("_OutlineThick", 0f);
                 lookingTo = null;
             }
 
-            if (hitted)
+            if (hitted && pickupPoint.childCount <= 0)
             {
                 lookingTo = hit.transform;
                 hit.transform.GetComponent<Renderer>().materials[1].SetFloat("_OutlineThick", 1.1f);
-            }
-            
-            interactionText.enabled = hitted && currentItemBody == null;
-
-            if (hitted && Input.GetMouseButtonDown(0) && currentItemBody == null)
-            {
-                currentItemBody = hit.transform.GetComponent<Rigidbody>();
-
-                currentItemBody.transform.SetParent(pickupPoint);
-                currentItemBody.velocity = Vector3.zero;
-                currentItemBody.useGravity = false;
-                currentItemBody.constraints = RigidbodyConstraints.FreezePosition;
-                currentItemBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
-                currentItemBody.position = pickupPoint.position;
-            }
-            else if (Input.GetMouseButtonDown(0) && currentItemBody != null)
-            {
-                currentItemBody.collisionDetectionMode = CollisionDetectionMode.Discrete;
-                currentItemBody.constraints = RigidbodyConstraints.None;
-                currentItemBody.transform.SetParent(null);
-                currentItemBody.useGravity = true;
-                currentItemBody = null;
             }
         }
     }
